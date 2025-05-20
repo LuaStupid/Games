0e88987a60de6bd46e82de41f7d8f8e3e1e7e17c
Hiru Hub <viplam479@gmail.com>
2025-05-20 07:09:22 +0700

    Add files via upload

---
+++ Games.lua
@@ -0,0 +1,10 @@
+local placeId = game. PlaceId
+if placeId == 2753915549 or placeId == 4442272183 or placeId == 7449423635 then
+--Blox Fruits
+getgenv().Team = "Pirates"
+getgenv().Chat = "Hiru Hub On Top"
+loadstring(game:HttpGet("https://raw.githubusercontent.com/LuaStupid/Games/main/BloxFruits.lua"))()
+elseif placeId == 14732615248 then
+--MemeSea
+loadstring(game:HttpGet("https://raw.githubusercontent.com/LuaStupid/Games/main/MemeSea.lua"))()
+end
