7f0ba36cf1d2941d607ceb41ccde3df57a20cacf
Hiru Hub <viplam479@gmail.com>
2025-06-20 08:33:57 +0700

    Update Games.lua

--- Games.lua
+++ Games.lua
@@ -1,7 +1,7 @@
 local placeId = game. PlaceId
 if placeId == 2753915549 or placeId == 4442272183 or placeId == 7449423635 then
 --Blox Fruits
-loadstring(game:HttpGet("https://raw.githubusercontent.com/LuaStupid/Games/main/BloxFruits.lua"))()
+loadstring(game:HttpGet("https://pandadevelopment.net/virtual/file/c58675a9d0605505"))()
 elseif placeId == 10260193230 then
 --MemeSea
 loadstring(game:HttpGet("https://raw.githubusercontent.com/LuaStupid/Games/main/MemeSea.lua"))()
